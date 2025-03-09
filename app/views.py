82bef8de72a6d94b14411e7de69e52bdf4bf1daa
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2025-03-09 21:05:48 +0300

    Добавили фитры и сортировки

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from .models import Product, Category
+from .models import Product, Category, Currency, City
 from django.views.generic import ListView, DetailView
-
+from django.db.models import Q
 
 class ProductListView(ListView):
     model = Product
@@ -28,13 +28,47 @@ class CategoryDetailView(ListView):
     
     def get_queryset(self):
         queryset = Product.objects.filter(category__slug=self.kwargs['category_slug'])
+        
+        # Search
         query = self.request.GET.get('q')
         if query:
-            queryset = queryset.filter(title__icontains=query)
+            queryset = queryset.filter(
+                Q(title__icontains=query) |
+                Q(description__icontains=query) 
+            )
+            
+        # Sorting
+        sort = self.request.GET.get('sort')
+        if sort == 'price_asc':
+            queryset = queryset.order_by('price')
+        elif sort == 'price_desc':
+            queryset = queryset.order_by('-price')
+        elif sort == 'date_desc':
+            queryset = queryset.order_by('-created_at')
+        elif sort == 'date_asc':
+            queryset = queryset.order_by('created_at')
+            
+        # City filter
+        city = self.request.GET.get('city')
+        if city:
+            queryset = queryset.filter(city__id=city)
+            
+        # Currency filter
+        currency = self.request.GET.get('currency')
+        if currency:
+            queryset = queryset.filter(currency__id=currency)
+            
         return queryset
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['category'] = Category.objects.get(slug=self.kwargs['category_slug'])
         context['query'] = self.request.GET.get('q', '')
+        context['current_sort'] = self.request.GET.get('sort', '')
+        context['current_city'] = self.request.GET.get('city', '')
+        context['current_currency'] = self.request.GET.get('currency', '')
+        context['cities'] = City.objects.all()
+        context['currencies'] = Currency.objects.all()
         return context
+
+
