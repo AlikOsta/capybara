23e34bd028c5f279ff034c67fee87c8e2ae20f53
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2025-03-09 20:56:31 +0300

    Летельное отображение списка объявлений конкретной категории с поисковой формой

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, CategoryDetailView
 
 app_name = 'app'
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
     path('product/<slug:product_slug>/', ProductDetailView.as_view(), name='product_detail'),
+    path('category/<slug:category_slug>/', CategoryDetailView.as_view(), name='category_detail'),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -13,8 +13,28 @@ class ProductListView(ListView):
         context['categories'] = Category.objects.all()
         return context
 
+
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'app/product_detail.html'
     context_object_name = 'product'
     slug_url_kwarg = 'product_slug'
+
+
+class CategoryDetailView(ListView):
+    model = Product
+    template_name = 'app/category_detail.html'
+    context_object_name = 'products'
+    
+    def get_queryset(self):
+        queryset = Product.objects.filter(category__slug=self.kwargs['category_slug'])
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(title__icontains=query)
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = Category.objects.get(slug=self.kwargs['category_slug'])
+        context['query'] = self.request.GET.get('q', '')
+        return context
