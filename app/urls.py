62136f0b25eb359960816f99d4f9028328bde79f
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2025-03-09 20:13:04 +0300

    Шаблон деталей объявления

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import ProductListView
+from .views import ProductListView, ProductDetailView
+
+app_name = 'app'
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
+    path('product/<slug:product_slug>/', ProductDetailView.as_view(), name='product_detail'),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Product, Category
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 class ProductListView(ListView):
@@ -12,3 +12,9 @@ class ProductListView(ListView):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
         return context
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'app/product_detail.html'
+    context_object_name = 'product'
+    slug_url_kwarg = 'product_slug'
