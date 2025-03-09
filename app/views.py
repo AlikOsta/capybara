559baeceeeed0e60c3a377eba1e6d48bf67c2c99
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2025-03-09 21:09:30 +0300

    подправил представление

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Product, Category, Currency, City
 from django.views.generic import ListView, DetailView
 from django.db.models import Q
+from django.shortcuts import get_object_or_404
 
 class ProductListView(ListView):
     model = Product
@@ -21,54 +22,54 @@ class ProductDetailView(DetailView):
     slug_url_kwarg = 'product_slug'
 
 
+
 class CategoryDetailView(ListView):
     model = Product
     template_name = 'app/category_detail.html'
     context_object_name = 'products'
     
     def get_queryset(self):
-        queryset = Product.objects.filter(category__slug=self.kwargs['category_slug'])
-        
-        # Search
+        queryset = Product.objects.filter(
+            category__slug=self.kwargs['category_slug']
+        ).select_related('category', 'city', 'currency')
+
+        # Поиск
         query = self.request.GET.get('q')
         if query:
             queryset = queryset.filter(
-                Q(title__icontains=query) |
-                Q(description__icontains=query) 
+                Q(title__icontains=query) | 
+                Q(description__icontains=query)
             )
-            
-        # Sorting
+        
+        # Сортировка
+        sort_options = {
+            'price_asc': 'price',
+            'price_desc': '-price',
+            'date_desc': '-created_at',
+            'date_asc': 'created_at'
+        }
         sort = self.request.GET.get('sort')
-        if sort == 'price_asc':
-            queryset = queryset.order_by('price')
-        elif sort == 'price_desc':
-            queryset = queryset.order_by('-price')
-        elif sort == 'date_desc':
-            queryset = queryset.order_by('-created_at')
-        elif sort == 'date_asc':
-            queryset = queryset.order_by('created_at')
-            
-        # City filter
-        city = self.request.GET.get('city')
-        if city:
-            queryset = queryset.filter(city__id=city)
-            
-        # Currency filter
-        currency = self.request.GET.get('currency')
-        if currency:
-            queryset = queryset.filter(currency__id=currency)
-            
-        return queryset
+        if sort in sort_options:
+            queryset = queryset.order_by(sort_options[sort])
+        
+        # Фильтры (город, валюта)
+        filters = {}
+        if city := self.request.GET.get('city'):
+            filters['city__id'] = city
+        if currency := self.request.GET.get('currency'):
+            filters['currency__id'] = currency
+
+        return queryset.filter(**filters)
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['category'] = Category.objects.get(slug=self.kwargs['category_slug'])
-        context['query'] = self.request.GET.get('q', '')
-        context['current_sort'] = self.request.GET.get('sort', '')
-        context['current_city'] = self.request.GET.get('city', '')
-        context['current_currency'] = self.request.GET.get('currency', '')
-        context['cities'] = City.objects.all()
-        context['currencies'] = Currency.objects.all()
+        context['category'] = get_object_or_404(Category, slug=self.kwargs['category_slug'])
+        context.update({
+            'query': self.request.GET.get('q'),
+            'current_sort': self.request.GET.get('sort'),
+            'current_city': self.request.GET.get('city'),
+            'current_currency': self.request.GET.get('currency'),
+            'cities': City.objects.all(),
+            'currencies': Currency.objects.all(),
+        })
         return context
-
-
