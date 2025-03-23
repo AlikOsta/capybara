8c1cf082d392c09f68a82045eb1ed7676f01b9f5
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2025-03-23 21:17:51 +0300

    init приложения для бота

---
+++ bot_capybara/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class BotCapybaraConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bot_capybara'
+    verbose_name = 'Telegram Бот'
