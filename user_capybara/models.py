e844b981e7d2d12cf028f868edbd6f1ec6560e65
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2025-03-24 04:19:56 +0300

    обновил медоль пользователя

--- user_capybara/models.py
+++ user_capybara/models.py
@@ -1,38 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
+from django.db.models import Avg
 
 
 class TelegramUserManager(BaseUserManager):
     """Менеджер для кастомной модели пользователя Telegram"""
     
-    def create_user(self, telegram_id, username=None, **extra_fields):
-        """Создает и сохраняет пользователя с указанным telegram_id и username"""
+    def create_user(self, telegram_id, username, password=None, **extra_fields):
+        """
+        Создаёт и сохраняет обычного пользователя с заданными telegram_id и username.
+        """
         if not telegram_id:
-            raise ValueError(_('Telegram ID обязателен'))
+            raise ValueError(_('The Telegram ID must be set'))
         
-        user = self.model(
-            telegram_id=telegram_id,
-            username=username,
-            **extra_fields
-        )
-        # Устанавливаем неиспользуемый пароль, т.к. авторизация через Telegram
-        user.set_unusable_password()
+        user = self.model(telegram_id=telegram_id, username=username, **extra_fields)
+        user.set_password(password)
         user.save(using=self._db)
         return user
-    
-    def create_superuser(self, telegram_id, username=None, **extra_fields):
-        """Создает и сохраняет суперпользователя с указанным telegram_id и username"""
+
+    def create_superuser(self, telegram_id, username, password, **extra_fields):
+        """
+        Создаёт суперпользователя с заданными telegram_id, username и паролем.
+        """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-        
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError(_('Суперпользователь должен иметь is_staff=True.'))
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError(_('Суперпользователь должен иметь is_superuser=True.'))
-        
-        return self.create_user(telegram_id, username, **extra_fields)
+
+        if not extra_fields.get('is_staff'):
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if not extra_fields.get('is_superuser'):
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+
+        return self.create_user(telegram_id, username, password, **extra_fields)
 
 
 class TelegramUser(AbstractUser):
@@ -40,10 +39,14 @@ class TelegramUser(AbstractUser):
     
     # Telegram-специфичные поля
     telegram_id = models.BigIntegerField(unique=True, verbose_name='Telegram ID')
+    username = models.CharField(max_length=30, unique=True, verbose_name='Имя пользователя')
     first_name = models.CharField(max_length=64, blank=True, null=True,verbose_name='Имя в Telegram')
     last_name = models.CharField(max_length=64, blank=True, null=True,verbose_name='Фамилия в Telegram')
     photo_url = models.URLField(blank=True, null=True,verbose_name='URL фото')
     auth_date = models.IntegerField(blank=True, null=True,verbose_name='Дата авторизации')
+
+    USERNAME_FIELD = 'telegram_id'
+    REQUIRED_FIELDS = ['username']
     
     # Указываем кастомный менеджер
     objects = TelegramUserManager()
@@ -57,20 +60,5 @@ class TelegramUser(AbstractUser):
             return f"@{self.username}"
         return f"User {self.telegram_id}"
     
-    def get_full_name(self):
-        """Возвращает полное имя пользователя"""
-        if self.first_name and self.last_name:
-            return f"{self.first_name} {self.last_name}"
-        elif self.first_name:
-            return self.first_name
-        elif self.username:
-            return f"@{self.username}"
-        return f"User {self.telegram_id}"
-    
-    def get_short_name(self):
-        """Возвращает короткое имя пользователя"""
-        if self.first_name:
-            return self.first_name
-        elif self.username:
-            return f"@{self.username}"
-        return f"User {self.telegram_id}"
+
+
