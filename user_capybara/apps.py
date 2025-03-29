0750916019a2fdaec47d9a72a92822273b311698
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2025-03-29 04:35:35 +0300

    обновил названия приложений

--- user_capybara/apps.py
+++ user_capybara/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class UserCapybaraConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_capybara'
+    verbose_name = 'Пользователи'
